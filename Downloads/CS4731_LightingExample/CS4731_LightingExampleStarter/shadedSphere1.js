da384315dd21b10c68188386fcab394ca7dcd40f
TheBigBryce <bclukens@wpi.edu>
2023-06-15 17:22:54 -0400

    sphere returns to original location when changing

--- Downloads/CS4731_LightingExample/CS4731_LightingExampleStarter/shadedSphere1.js
+++ Downloads/CS4731_LightingExample/CS4731_LightingExampleStarter/shadedSphere1.js
@@ -1,41 +1,41 @@
 var canvas;
 var gl;
+var program;
 
 var numTimesToSubdivide = 0;
 var numTimesToSubdivideCurve = 0;
-
+var translateMat = translate(0,0,0);
 var index = 0;
 var pointsArray = [];
 var normalsArray = [];
 var flatShadingArray = [];
-
+let j = 1;
+let lastLocations = [];
 var near = .1;
 var far = 100;
 let animate = false;
 
 let lineControlPoints = [
-    vec4(0, 0, 1.0, 1.0),
-    vec4(-0.75, -0.5, 0.0, 1.0),
-    vec4(-0.25, 0.5, 1.0, 1.0),
-    vec4(0.25, 0.5, 1.0, 1.0),
-    vec4(0.75, -0.5, 1.0, 1.0),
-    vec4(1, 1, 0.0, 1.0),
-    //  vec4(20, 20, 0.0, 1.0),
-    //  vec4(10, 25, 0.0, 1.0),
-    //  vec4(0, 20, 0.0, 1.0),
-    // vec4(10, 25, 0.0, 1.0),
+    vec4(0,0,0,1.0),
+    vec4(-2, 3, 1.0, 1.0),
+    vec4(10, 3, 1.0, 1.0),
+    vec4(16.5, -3, 1.0, 1.0),
+    vec4(8, -9, 1.0, 1.0),
+    vec4(14, -15, 1.0, 1.0),
+    vec4(0, -15, 1.0, 1.0),
+    vec4(-2, -8, 1.0, 1.0),
 ];
 
 //side 1
-var va = normalize(vec4(1.0, 1.0, 1.0,1));
-var vb = normalize(vec4(-1.0, -1.0, 1.0, 1));
-var vc = normalize(vec4(-1.0, 1.0, 1.0, 1));
-var vd = normalize(vec4(1.0, -1.0, 1.0, 1));
+var va = normalize(vec4(.5, .5, .5, 1));
+var vb = normalize(vec4(-.5, -.5, .5, 1));
+var vc = normalize(vec4(-.5, .5, .5, 1));
+var vd = normalize(vec4(.5, -.5, .5, 1));
 //side 2
-var ve = normalize(vec4(1.0, -1.0, -1.0, 1));
-var vf = normalize(vec4(-1.0, -1.0, -1.0, 1));
-var vg = normalize(vec4(-1.0, 1.0, -1.0, 1));
-var vh = normalize(vec4(1.0, 1.0, -1.0, 1));
+var ve = normalize(vec4(.5, -.5, -.5, 1));
+var vf = normalize(vec4(-.5, -.5, -.5, 1))
+var vg = normalize(vec4(-.5, .5, -.5, 1));
+var vh = normalize(vec4(.5, .5, -.5, 1));
 
 var lightPosition = vec4(3.0, 1.0, 1.0, 0.0 );
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
@@ -51,28 +51,28 @@ var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
 
 var eye;
-var at = vec3(0.0, 0.0, 0.0);
+var at = vec3(7.0, -6.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
 function triangle(a, b, c) {
 
 
 
-     pointsArray.push(a);
-     pointsArray.push(b);
-     pointsArray.push(c);
+    pointsArray.push(a);
+    pointsArray.push(b);
+    pointsArray.push(c);
 
-     flatShadingArray.push(a);
-     flatShadingArray.push(a);
-     flatShadingArray.push(a);
+    flatShadingArray.push(a);
+    flatShadingArray.push(a);
+    flatShadingArray.push(a);
 
-     // normals are vectors
+    // normals are vectors
 
-     normalsArray.push(a[0],a[1], a[2], 0.0);
-     normalsArray.push(a[0],a[1], a[2], 0.0);
-     normalsArray.push(a[0],a[1], a[2], 0.0);
+    normalsArray.push(a[0],a[1], a[2], 0.0);
+    normalsArray.push(a[0],a[1], a[2], 0.0);
+    normalsArray.push(a[0],a[1], a[2], 0.0);
 
-     index += 3;
+    index += 3;
 
 }
 
@@ -90,13 +90,18 @@ function onkeypress(event){
         animate = !animate;
     }
     if(event.key==="i"||event.key === "I"){
-        if(numTimesToSubdivideCurve !== 8)
-            numTimesToSubdivideCurve+=1;
+        if(numTimesToSubdivideCurve !== 8) {
+            lastLocations[numTimesToSubdivideCurve]=j;
+            numTimesToSubdivideCurve += 1;
+        }
     }
     if(event.key==="j"||event.key === "J"){
-        if(numTimesToSubdivideCurve !== 0)
-            numTimesToSubdivideCurve= numTimesToSubdivideCurve-1;
-        console.log(numTimesToSubdivideCurve);
+        if(numTimesToSubdivideCurve !== 0) {
+            numTimesToSubdivideCurve -= 1;
+            if(j!==0) {
+                j=lastLocations[numTimesToSubdivideCurve];
+            }
+        }
     }
     pointsArray = [];
     normalsArray = [];
@@ -139,22 +144,23 @@ window.addEventListener('keypress',onkeypress,false);
 window.onload =
     function init() {
 
-    canvas = document.getElementById( "gl-canvas" );
-    gl = WebGLUtils.setupWebGL( canvas,undefined);
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+        canvas = document.getElementById( "gl-canvas" );
+        gl = WebGLUtils.setupWebGL( canvas,undefined);
+        if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+        gl.viewport( 0, 0, canvas.width, canvas.height );
+        gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
 
-    gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.DEPTH_TEST);
 
-   setup();
-}
+        setup();
+    }
+    var id;
 function setup(){
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
     divideSquare();
@@ -166,6 +172,7 @@ function setup(){
     var vPosition = gl.getAttribLocation( program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+
 
     var vNormal = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vNormal);
@@ -182,7 +189,6 @@ function setup(){
     var specularProduct = mult(lightSpecular, materialSpecular);
     var ambientProduct = mult(lightAmbient, materialAmbient);
 
-
     gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));
     gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));
     gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
@@ -192,12 +198,33 @@ function setup(){
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-    gl.uniform1f(gl.getUniformLocation(program,"a"),1.0);
+    eye = vec3(7,-7, 40);
 
-    render();
+    modelViewMatrix = lookAt(eye, at , up);
+    var fovy = 30;
+    projectionMatrix = perspective(fovy,1,near,far);
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
 
     gl.uniform1f(gl.getUniformLocation(program,"a"),0.0);
     let linePoints =  chaikin(lineControlPoints, numTimesToSubdivideCurve);
+
+    let currentLine=linePoints[j];
+    translateMat = translate(currentLine[0],currentLine[1],currentLine[2]);
+
+    if(animate){
+        if(j===linePoints.length){
+            j=0;
+        }
+        else{
+            j++;
+        }
+        id=requestAnimationFrame(setup);
+        let currentNormal = normalize(currentLine);
+        translateMat = translate(currentNormal[0],currentNormal[1],currentNormal[2]);
+        translateMat=translate(j,j,j);
+    }
 
     var vBuff = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuff);
@@ -209,32 +236,16 @@ function setup(){
 
     gl.drawArrays( gl.LINE_LOOP, 0, linePoints.length);
 
-
-
-}
-function render() {
-
-   // gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    eye = vec3(5,5, 40);
-
-    modelViewMatrix = lookAt(eye, at , up);
-    var fovy = 30;
-    projectionMatrix = perspective(fovy,1,near,far);
-
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+    gl.uniform1f(gl.getUniformLocation(program,"a"),1.0);
+    var ctMatrixLoc = gl.getUniformLocation(program, "translate");
+    gl.uniformMatrix4fv(ctMatrixLoc, false, flatten(translateMat));
     for( var i=0; i<index; i+=3) {
         gl.drawArrays(gl.TRIANGLES, i, 3);
     }
-   //requestAnimationFrame(render);
+
+
 }
 
-function animateSquare(){
-    if(animate()){
-
-    }
-}
 
 function divideSquare(){
     square(va, vb, vc, vd, numTimesToSubdivide);
@@ -270,8 +281,8 @@ function chaikin(vertices, iterations) {
         var p1 = mix(v0, v1, (1.0 - ratio));
         newVertices.push(p0, p1);
     }
-    var v0 = vertices[0];
-    var v1 = vertices[vertices.length - 1];
+    var v0 = vertices[vertices.length - 1];
+    var v1 = vertices[0];
 
     // Cut vertices and add to list
     // Calculate first new point
